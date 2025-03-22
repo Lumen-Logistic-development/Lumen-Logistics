09ead65ea6e7a4f751989262df5d8dacad760687
JayWebtech <jethroadamzy@gmail.com>
2025-03-22 21:38:10 +0100

    feat : recent transactions components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import RecentTransactions from '@/components/RecentTransactions';
 
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <main className="min-h-screen bg-[#121212] p-8">
+      <div className="max-w-5xl mx-auto">
+        <RecentTransactions />
+      </div>
+    </main>
   );
 }
