fdfe23b22d9bfa1e0439fe29ba3956d324ca1b75
Fahmed2024 <100038212+Fahmedo@users.noreply.github.com>
2025-03-22 14:56:14 +0100

    Design of product status chart compoenent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import { InventoryChart } from "./components/inventory-chart";
+import { InventoryChart } from './components/inventory-chart';
+import { ProductionStatusChart } from './components/production-status-chart';
 
 export default function Home() {
   return (
     <div className="">
-      Home
-      <div className=" w-1/2">
-        <InventoryChart />
+      <div className="text-center my-5"> Home page</div>
+      <div className="grid grid-cols-7">
+        <div className="col-span-2">sidebar</div>
+        <div className=" col-span-5 grid gap-5 grid-cols-4 max-w-[1400px] ">
+          <div className="col-span-2">
+            <InventoryChart />
+          </div>
+
+          <div className="col-span-2">
+            <ProductionStatusChart />
+          </div>
+        </div>
       </div>
     </div>
   );
