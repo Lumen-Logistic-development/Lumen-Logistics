c3455d9c6fd1f962aae0a48cca09fd40a1f68ed2
Faruq Ahmed Olaitan <100038212+Fahmedo@users.noreply.github.com>
2025-03-24 23:24:40 +0100

    design of Inventory Levels Chart (#14)
    * design of Inventory Levels Chart
    
    * Design of product status chart compoenent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { InventoryChart } from './components/inventory-chart';
+import { ProductionStatusChart } from './components/production-status-chart';
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className="">
+      <div className="text-center my-5"> Home page</div>
+      <div className="grid grid-cols-7">
+        <div className="col-span-2">sidebar</div>
+        <div className=" col-span-5 grid gap-5 grid-cols-4 max-w-[1400px] ">
+          <div className="col-span-2">
+            <InventoryChart />
+          </div>
+
+          <div className="col-span-2">
+            <ProductionStatusChart />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
