eb1e72dd776949689d40726673b055269e915b68
Kom02 <kom0211@gmail.com>
2025-03-25 18:33:55 -0600

    Fix merge (#19)
    * fix-merge
    
    * Merge conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-[#121212] p-8">
       <div className="">
-        <div className="text-center my-5"> Home page</div>
         <div className="grid grid-cols-7">
-          <div className="col-span-2">sidebar</div>
           <div className=" col-span-5 grid gap-5 grid-cols-4 max-w-[1400px] ">
             <div className="col-span-2">
               <InventoryChart />
