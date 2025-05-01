7db64bbcc35daaa51a3d70df611b16b0f0190442
Austine Blaise <52866711+austineblaise@users.noreply.github.com>
2025-05-01 03:26:53 +0100

    feat: integrate missing components into dashboard for complete user experience (#38)

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import DashboardPage from '@/components/Dashboard/Dashboard';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <DashboardPage />
+        </div>
+    );
+};
+
+export default page;
